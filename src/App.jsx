57fbb3ffb9655992e49ef8a9cb0d37e83dbd07bc
Vlad Caye <99850723+vladcaye@users.noreply.github.com>
2026-04-05 18:49:26 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import { Navbar } from './sections/Navbar'
+import { Home } from './sections/Home'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <BrowserRouter>
+    <main className="min-h-screen w-full max-w-full overflow-x-hidden">
+      <Navbar />
+      {/* <Home /> */}
+    </main>
+    </BrowserRouter>
+      
+    </>
+  )
+}
+
+export default App
