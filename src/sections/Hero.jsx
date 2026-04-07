551b5e027c93365247427ea8d936dd18a6200f8e
Vlad Caye <99850723+vladcaye@users.noreply.github.com>
2026-04-07 10:48:49 -0400

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -21,7 +21,7 @@ export const Hero = () => {
     const isMobile = useMediaQuery({ maxWidth: 853 })
     return (
         <section id="hero" className="flex items-start justify-center 
-        md:items-start md:justify-start min-h-screen overflow-hidden c-space w-full">
+        md:items-start md:justify-start min-h-screen overflow-x-hidden c-space w-full">
             <div className="absolute top-1/2 left-1/2
             w-200 h-200 bg-primary/5
             rounded-full blur-3xl -translate-x-1/2 -translate-y-1/2"
