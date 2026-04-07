d1b0aaf8bc302a05de2052daa737e022821672ee
Vlad Caye <99850723+vladcaye@users.noreply.github.com>
2026-04-07 10:18:54 -0400

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -78,21 +78,21 @@ export const Hero = () => {
                     {/* Left Column - Text Content */}
                     <div className="space-y-8 text-center h-full">
                         {/* Hero */}
-                        <div className="space-y-10 md:space-y-0 animate-fade-in animation-delay-100">
+                        {/* <div className="space-y-10 md:space-y-0 animate-fade-in animation-delay-100">
                             <Suspense fallback={<Loader />}>
                                 <HeroText />
                             </Suspense>
                             
-                        </div>
+                        </div> */}
 
                         {/* CTA */}
-                        <div className="flex flex-wrap justify-center gap-4 animate-fade-in animation-delay-300">
+                        {/* <div className="flex flex-wrap justify-center gap-4 animate-fade-in animation-delay-300">
                             <a href="https://www.instagram.com/leconeorange" target="_blank">
                                 <Button size="lg">
                                     SHOP <ShoppingCart className="w-5 h-5" />
                                 </Button>
                             </a>
-                        </div>
+                        </div> */}
                         {/* CTA */}
 
 
@@ -101,7 +101,7 @@ export const Hero = () => {
                 </div>
 
                 {/* Services */}
-                <div className="mt-10 animate-fade-in animation-delay-600">
+                {/* <div className="mt-10 animate-fade-in animation-delay-600">
                     <p className="text-sm text-white/60 hover:text-white text-center uppercase">WHAT WE DO</p>
                     <div className="relative overflow-hidden">
                         <div className="flex animate-marquee">
@@ -114,11 +114,11 @@ export const Hero = () => {
                             ))}
                         </div>
                     </div>
-                </div>
+                </div> */}
                 {/* Services */}
 
                 {/* ChevronDown */}
-                <div className='absolute bottom-8 left-1/2 -translate-x-1/2
+                {/* <div className='absolute bottom-8 left-1/2 -translate-x-1/2
             animate-fade-in animation-delay-800'>
                     <a
                         href="#about"
@@ -126,7 +126,7 @@ export const Hero = () => {
                         <span className='text-primary hover:text-white text-sm uppercase tracking-wider'>Scroll</span>
                         <ChevronDown className='text-primary hover:text-white w-6 h-6 animate-bounce' />
                     </a>
-                </div>
+                </div> */}
             </div >
 
 
