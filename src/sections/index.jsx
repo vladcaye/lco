ba7399fcd545e598b02a73d05b15407ec40a7578
Vlad Caye <99850723+vladcaye@users.noreply.github.com>
2026-04-07 14:19:02 -0400

    Create index.jsx

---
+++ src/sections/index.jsx
@@ -0,0 +1,7 @@
+import { Hero } from './Hero'
+import { Home } from './Home'
+import { About } from './About'
+import { Cornerstore } from './Cornerstore'
+import { Contact } from './Contact'
+
+export { Hero, Home, About, Cornerstore, Contact }
